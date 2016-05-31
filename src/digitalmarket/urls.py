6a57a1bed80523b8bdb5f491e210f96fbfe62571
Nhan Tran <nhantrantnt@gmail.com>
2016-05-30 23:15:25 -0600

    set up urls, created templates and inheritance, added url dynamic patterns

--- src/digitalmarket/urls.py
+++ src/digitalmarket/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^detail/(?P<object_id>\d+)$', "products.views.detail_view", name="detail_view"),
+    url(r'^list/$', "products.views.list_view", name="list_view")
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 # Create your views here.
+def detail_view(request, object_id=None):
+    #1 item
+    product = get_object_or_404(Product, id=object_id)
+    template = "detail_view.html"
+    context={
+        "object": product
+    }
+    return render(request, template, context)
+
+def list_view(request):
+    #list of items
+    print request
+    queryset = Product.objects.all()
+    template = "list_view.html"
+    context={
+        "queryset": queryset
+    }
+    return render(request, template, context)
+
+# def detail_view(request):
+#     print request
+#     template = "detail_view.html"
+#     context={}
+#     return render(request, template, context)
+#
+# def detail_view(request):
+#     print request
+#     template = "detail_view.html"
+#     context={}
+#     return render(request, template, context)
+#
+
+
