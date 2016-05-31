5be94bb40ca18b7c7ebbe8c85ecfc676881a35b2
Nhan Tran <nhantrantnt@gmail.com>
2016-05-30 22:20:22 -0600

    added ProductAdmin and set up migrations

--- src/products/models.py
+++ src/products/models.py
@@ -6,6 +6,10 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=30)
+    description = models.TextField(default="Default value")
+    price = models.DecimalField(max_digits=100, decimal_places=2, default=9.99)
+    sale_price = models.DecimalField(max_digits=100, decimal_places=2, default=6.99, null=True, blank=True)
+
    # item_id = models.IntegerField()
 
     def __unicode__(self):
