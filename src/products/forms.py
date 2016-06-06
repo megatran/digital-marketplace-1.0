3ecb9b31ea731e3b5c6fab1cad7171994981d366
Nhan Tran <nhantrantnt@gmail.com>
2016-06-05 23:57:24 -0600

    added update view

--- src/products/forms.py
+++ src/products/forms.py
@@ -1,6 +1,87 @@
 from django import forms
+from .models import Product
+
+PUBLISH_CHOICES = (
+   # ('', ""),
+    ('publish', "Publish"),
+    ('draft', "Draft")
+)
 
 class ProductAddForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
+    title = forms.CharField(label="Your Title", widget=forms.TextInput(
+        attrs={
+            "class": "custom-class",
+            "placeholder": "Title"
+        }))
+    description = forms.CharField(widget=forms.Textarea(
+        attrs={
+            "class": "my-custo-class",
+            "placeholder": "Description",
+            "some-attr": "haha",
+        }
+    ))
     price = forms.DecimalField()
+    publish = forms.ChoiceField(widget=forms.RadioSelect, choices=PUBLISH_CHOICES, required=False)
+
+    def clean_price(self):
+        price = self.cleaned_data.get("price")
+        if price <= 1.00 :
+            raise forms.ValidationError("Price must be greater than 1")
+        elif price >= 99.99:
+            raise forms.ValidationError("Price must be less than 100")
+        else:
+            return price
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if len(title) >= 3:
+            return title
+        else:
+            raise forms.ValidationError("Title must be greater than 3 characters long")
+
+class ProductModelForm(forms.ModelForm):
+    publish = forms.ChoiceField(widget=forms.RadioSelect, choices=PUBLISH_CHOICES, required=False)
+
+    title = forms.CharField(label="Your Title", widget=forms.TextInput(
+        attrs={
+            "class": "custom-class",
+            "placeholder": "Title"
+        }))
+    description = forms.CharField(widget=forms.Textarea(
+        attrs={
+            "class": "my-custo-class",
+            "placeholder": "Description",
+            "some-attr": "haha",
+        }
+    ))
+
+
+    class Meta:
+        model = Product
+        fields = [
+            "title",
+            "description",
+            "price",
+        ]
+        # widgets = {
+        #     "title": forms.TextInput(
+        #         attrs={
+        #             "placeholder": "Title"
+        #         }
+        #     )
+        # }
+    def clean_price(self):
+        price = self.cleaned_data.get("price")
+        if price <= 1.00 :
+            raise forms.ValidationError("Price must be greater than 1")
+        elif price >= 100.00:
+            raise forms.ValidationError("Price must be less than 100")
+        else:
+            return price
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if len(title) >= 3:
+            return title
+        else:
+            raise forms.ValidationError("Title must be greater than 3 characters long")
--- src/products/views.py
+++ src/products/views.py
@@ -1,23 +1,30 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Product
-from .forms import ProductAddForm
+from .forms import ProductAddForm, ProductModelForm
 # Create your views here.
 
 def create_view(request):
-    #FORM
-    form = ProductAddForm(request.POST or None)
+    #ProductAddFORM
+    # form = ProductAddForm(request.POST or None)
+    # if form.is_valid():
+    #     data = form.cleaned_data
+    #     title = data.get("title")
+    #     description = data.get("description")
+    #     price = data.get("price")
+    #     #new_obj = Product.objects.create(title=title, description=description, price=price)
+    #     new_obj = Product()
+    #     new_obj.title = title
+    #     new_obj.description = description
+    #     new_obj.price = price
+    #     new_obj.save()
+
+    #PRODUCT MODEL FORM method:
+    form = ProductModelForm(request.POST or None)
     if form.is_valid():
-        data = form.cleaned_data
-        title = data.get("title")
-        description = data.get("description")
-        price = data.get("price")
-        #new_obj = Product.objects.create(title=title, description=description, price=price)
-        new_obj = Product()
-        new_obj.title = title
-        new_obj.description = description
-        new_obj.price = price
-        new_obj.save()
+        instance = form.save(commit=False)
+        instance.sale_price = instance.price
+        instance.save()
     template = "create_view.html"
     context = {
         "form": form,
@@ -45,6 +52,21 @@ def detail_view(request, object_id=None):
         "object": product
     }
     return render(request, template, context)
+
+def update_view(request, object_id=None):
+    #1 item
+    product = get_object_or_404(Product, id=object_id)
+    form = ProductModelForm(request.POST or None, instance=product)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+    template = "update_view.html"
+    context={
+        "object": product,
+        "form": form,
+    }
+    return render(request, template, context)
+
 
 def list_view(request):
     #list of items
